f3c70849bf7d3faca2d26edb7a2232dd9525c9a4
AlaibaNawaz <137944285+AlaibaNawaz@users.noreply.github.com>
2024-02-06 01:30:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas 
 import altair 
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
